ee043f39a08c5d609018cbd192b22e6bc717ad24
Kunal Saha <kunalsaha1107@gmail.com>
2025-04-26 12:24:04 +0530

    feat: products model and table

---
+++ src/main/java/org/kunal/ECommerce/model/Product.java
@@ -0,0 +1,31 @@
+package org.kunal.ECommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String Category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+}

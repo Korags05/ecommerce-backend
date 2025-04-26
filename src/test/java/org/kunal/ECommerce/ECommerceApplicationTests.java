f0399d71ceb529ea999b939f4684ad1d4f799e0f
Kunal Saha <kunalsaha1107@gmail.com>
2025-04-26 11:41:23 +0530

    init

---
+++ src/test/java/org/kunal/ECommerce/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kunal.ECommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

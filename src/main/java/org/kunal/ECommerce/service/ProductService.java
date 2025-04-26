e23cf456b0dda952acfeb962442e2931841d30eb
Kunal Saha <kunalsaha1107@gmail.com>
2025-04-26 12:42:50 +0530

    feat: service, controller and repo

---
+++ src/main/java/org/kunal/ECommerce/service/ProductService.java
@@ -0,0 +1,19 @@
+package org.kunal.ECommerce.service;
+
+import org.kunal.ECommerce.model.Product;
+import org.kunal.ECommerce.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+}
